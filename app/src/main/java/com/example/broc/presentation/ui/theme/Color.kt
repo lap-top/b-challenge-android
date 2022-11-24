6099cbbfd5f5a794a377c470386ef382484735b0
lap-top <49708840+lap-top@users.noreply.github.com>
2022-11-25 04:31:56 +1100

    tweaks

--- app/src/main/java/com/example/broc/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/example/broc/presentation/ui/theme/Color.kt
@@ -2,9 +2,6 @@ package com.example.broc.presentation.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
 
 // Light
 val backgroundGreen = Color(0xFF305E3A)
--- app/src/main/java/com/example/broc/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/example/broc/presentation/ui/theme/Theme.kt
@@ -7,7 +7,6 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
@@ -15,17 +14,16 @@ import androidx.core.view.ViewCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = backgroundGreen,
-    secondary = lightGreen,
-    tertiary = Pink80,
-    background = backgroundGreen,
+    onPrimary = lightGreen,
+    secondary = primaryGreen,
+    tertiary = Pink40,
 )
 
 private val LightColorScheme = lightColorScheme(
     primary = backgroundGreen,
     onPrimary = lightGreen,
-    secondary = Color(0xFFD1FFA4),
+    secondary = primaryGreen,
     tertiary = Pink40,
-
     )
 
 @Composable
