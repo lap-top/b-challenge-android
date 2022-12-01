f00a9cfad5c919a09624593a95b148e75cf62743
lap-top <s3716716@student.rmit.edu.au>
2022-12-01 14:53:39 +1100

    Xml Rewrite base

--- app/src/main/java/com/example/broc/presentation/MainActivity.kt
+++ app/src/main/java/com/example/broc/presentation/MainActivity.kt
@@ -1,31 +1,37 @@
 package com.example.broc.presentation
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
-import com.example.broc.presentation.ui.theme.BrocTheme
+import androidx.activity.viewModels
 import dagger.hilt.android.AndroidEntryPoint
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.example.broc.databinding.ActivityMainBinding
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            BrocTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
 
-                    Navigation()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        lifecycleScope.launch {
+            viewModel.state.collect { state ->
+                when (state) {
+                    is MainViewModel.MainState.Error -> binding.btnTest.text = "abc"
+                    is MainViewModel.MainState.Success -> binding.btnTest.text = state.data.toString()
                 }
+
             }
         }
+       // val navController = findNavController(R.id.nav_host_fragment_content_main)
+
     }
+
 }
